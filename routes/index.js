d2b5db5e27c4a93ad3044651cabe2c1ce47b5543
Carlos Ort-Patrick <27COrtPatrick@live.ytech.edu>
2026-08-31 08:49:30 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureLoggedIn } = require('../middleware/auth');
 
-router.get('/', ensureLoggedIn, (req, res) => {
+router.get('/', (req, res) => {
     res.render('index', { user: req.user });
 });
 
